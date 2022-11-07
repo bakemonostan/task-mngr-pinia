5a9dc5a5c0e940769df35644592e7e9117d40837
bakemonostan <ehizomokhomion@gmail.com>
2022-11-06 16:28:50 -0800

    cleared templated, added header and styles.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div></div>
+  <main>
+    <header>
+      <img src="./assets/logo.svg" alt="pinia logo" />
+      <h1>Pinia Logo</h1>
+    </header>
+  </main>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped></style>
+
